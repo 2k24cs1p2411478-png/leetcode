7c73e127c495250c0e379d02b9a87c825a1e8a11
Srashti Rathore <2k24.cs1p.2411478@gmail.com>
2026-09-12 20:14:23 +0530

    Time: 1 ms (96.47%), Space: 43.8 MB (40.41%) - LeetHub

---
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        int[] count = new int[101];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                count[nums[i]]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 1) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
